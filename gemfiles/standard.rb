13477cb0838508e741e5809e6c5dc368545b97e5
johnnyshields <johnny.shields@gmail.com>
2023-04-11 03:26:29 +0900

    Remove more cruft gems

--- gemfiles/standard.rb
+++ gemfiles/standard.rb
@@ -3,19 +3,6 @@ def standard_dependencies
 
   group :development do
     gem 'yard'
-
-    platform :mri do
-      # Debugger for VSCode.
-      if !ENV['CI'] && !ENV['DOCKER'] && RUBY_VERSION < '3.0'
-        gem 'debase'
-        gem 'ruby-debug-ide'
-      end
-    end
-
-    # Evergreen configuration generation
-    gem 'erubi'
-    gem 'tilt'
-    gem 'solargraph'
   end
 
   group :development, :test do
@@ -24,14 +11,6 @@ def standard_dependencies
     gem 'rubocop-rails', '~> 2.17.4'
     gem 'rubocop-rake', '~> 0.6.0'
     gem 'rubocop-rspec', '~> 2.18.1'
-
-    platform :mri do
-      gem 'byebug'
-    end
-
-    platform :jruby do
-      gem 'ruby-debug'
-    end
   end
 
   group :test do
