f93058c74aacf38fbc02b7ae6532b61d805631d2
johnnyshields <johnny.shields@gmail.com>
2023-04-16 10:25:25 +0900

    More cleanup

--- spec/support/constraints.rb
+++ spec/support/constraints.rb
@@ -3,25 +3,6 @@
 require 'i18n'
 
 module Constraints
-  RAILS_VERSION = ActiveSupport.version.to_s.split('.')[0..1].join('.').freeze
-
-  # This is a macro for retrying flaky tests on CI that occasionally fail.
-  # Note that the tests will only be retried on CI.
-  #
-  # @param [ Integer ] :tries The number of times to retry.
-  # @param [ Integer ] :sleep The number of seconds to sleep in between retries.
-  #   If nothing, or nil, is passed, we won't wait in between retries.
-  def retry_test(tries: 3, sleep: nil)
-    return unless %w[1 yes true].include?(ENV['CI'])
-
-    around do |example|
-      if sleep
-        example.run_with_retry retry: tries, retry_wait: sleep
-      else
-        example.run_with_retry retry: tries
-      end
-    end
-  end
 
   class I18nBackendWithFallbacks < ::I18n::Backend::Simple
     include ::I18n::Backend::Fallbacks
@@ -47,114 +28,32 @@ module Constraints
     end
   end
 
+  [
+    [:ruby, 'Ruby', RUBY_VERSION],
+    [:rails, 'Rails', ActiveSupport.version.to_s],
+    [:driver, 'Mongo Driver', Mongo::VERSION],
+    [:bson, 'BSON Ruby', BSON::VERSION],
+    [:server, 'MongoDB Server', -> { ClusterConfig.instance.server_version }],
+    [:fcv, 'MongoDB Server FCV', -> { ClusterConfig.instance.fcv_ish }],
+    [:libmongocrypt, 'libmongocrypt', -> { Mongo::Crypt::Binding.mongocrypt_version(nil) }]
+  ].each do |meth, name, current_version|
+    %i[min max].each do |dir|
+      define_method(:"#{dir}_#{meth}_version") do |version|
+        current_version = current_version.call if current_version.is_a?(Proc)
+        before(:all) do
+          if Constraints.send(:"#{dir}_version?", current_version, version)
+            skip "#{name} version #{version} or #{dir == :min ? 'higher' : 'lower'} " \
+                 "is required (using #{current_version})"
+          end
+        end
+      end
+    end
+  end
+
   def require_mri
     before(:all) do
       unless SpecConfig.instance.mri?
         skip "MRI required, we have #{SpecConfig.instance.platform}"
-      end
-    end
-  end
-
-  def min_driver_version(version)
-    before(:all) do
-      if Constraints.min_version?(Mongo::VERSION, version)
-        skip "Driver version #{version} or higher is required"
-      end
-    end
-  end
-
-  def max_driver_version(version)
-    before(:all) do
-      if Constraints.max_version?(Mongo::VERSION, version)
-        skip "Driver version #{version} or lower is required"
-      end
-    end
-  end
-
-  def min_bson_version(version)
-    before(:all) do
-      if Constraints.min_version?(BSON::VERSION, version)
-        skip "bson-ruby version #{version} or higher is required"
-      end
-    end
-  end
-
-  def max_bson_version(version)
-    before(:all) do
-      if Constraints.max_version?(BSON::VERSION, version)
-        skip "bson-ruby version #{version} or lower is required"
-      end
-    end
-  end
-
-  def min_ruby_version(version)
-    before(:all) do
-      if Constraints.min_version?(RUBY_VERSION, version)
-        skip "Ruby version #{version} or higher required, we have #{RUBY_VERSION}"
-      end
-    end
-  end
-
-  def max_ruby_version(version)
-    before(:all) do
-      if Constraints.max_version?(RUBY_VERSION, version)
-        skip "Ruby version #{version} or higher required, we have #{RUBY_VERSION}"
-      end
-    end
-  end
-
-  def min_rails_version(version)
-    before(:all) do
-      if Constraints.min_version?(RAILS_VERSION, version)
-        skip "Rails version #{version} or higher required, we have #{RAILS_VERSION}"
-      end
-    end
-  end
-
-  def max_rails_version(version)
-    before(:all) do
-      if Constraints.max_version?(RAILS_VERSION, version)
-        skip "Rails version #{version} or lower required, we have #{RAILS_VERSION}"
-      end
-    end
-  end
-
-  def min_server_version(version)
-    current_version = ClusterConfig.instance.server_version
-
-    before(:all) do
-      if Constraints.min_version?(current_version, version)
-        skip "Server version #{version} or higher required, we have #{current_version}"
-      end
-    end
-  end
-
-  def max_server_version(version)
-    current_version = ClusterConfig.instance.server_version
-
-    before(:all) do
-      if Constraints.max_version?(current_version, version)
-        skip "Server version #{version} or lower required, we have #{current_version}"
-      end
-    end
-  end
-
-  def min_server_fcv(version)
-    current_version = ClusterConfig.instance.fcv_ish
-
-    before(:all) do
-      if Constraints.min_version?(current_version, version)
-        skip "FCV #{version} or higher required, we have #{current_version} (server #{ClusterConfig.instance.server_version})"
-      end
-    end
-  end
-
-  def max_server_fcv(version)
-    current_version = ClusterConfig.instance.fcv_ish
-
-    before(:all) do
-      if Constraints.max_version?(current_version, version)
-        skip "FCV #{version} or lower required, we have #{current_version} (server #{ClusterConfig.instance.server_version})"
       end
     end
   end
@@ -178,15 +77,9 @@ module Constraints
     before(:all) do
       case ClusterConfig.instance.topology
       when :single
-        skip 'Transactions tests require a replica set (4.0+) or a sharded cluster (4.2+)'
-      when :replica_set
-        unless ClusterConfig.instance.server_version >= '4.0'
-          skip 'Transactions tests in a replica set topology require server 4.0+'
-        end
-      when :sharded, :load_balanced
-        unless ClusterConfig.instance.server_version >= '4.2'
-          skip 'Transactions tests in a sharded cluster topology require server 4.2+'
-        end
+        skip 'Transactions tests require a replica set or a sharded cluster'
+      when :replica_set, :sharded, :load_balanced
+        # ok
       else
         raise NotImplementedError
       end
@@ -239,28 +132,6 @@ module Constraints
       # include FLE therefore run the FLE tests.
       unless ENV['LIBMONGOCRYPT_PATH'].present? || ENV['FLE'].present?
         skip 'Test requires path to libmongocrypt to be specified in LIBMONGOCRYPT_PATH env variable'
-      end
-    end
-  end
-
-  def min_libmongocrypt_version(version)
-    require_libmongocrypt
-    current_version = Mongo::Crypt::Binding.mongocrypt_version(nil)
-
-    before(:all) do
-      if Constraints.min_version?(current_version, version)
-        skip "libmongocrypt version #{version} required, but version #{current_version} is available"
-      end
-    end
-  end
-
-  def max_libmongocrypt_version(version)
-    require_libmongocrypt
-    current_version = Mongo::Crypt::Binding.mongocrypt_version(nil)
-
-    before(:all) do
-      if Constraints.max_version?(current_version, version)
-        skip "libmongocrypt version #{version} required, but version #{current_version} is available"
       end
     end
   end
