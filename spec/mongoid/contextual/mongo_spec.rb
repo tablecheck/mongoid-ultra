336b14b4994591cacd154af0ccc85ba703986c5c
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2023-08-27 16:28:36 +0900

    Fix rubocop, get rid of extra loop

--- spec/mongoid/contextual/mongo_spec.rb
+++ spec/mongoid/contextual/mongo_spec.rb
@@ -4642,9 +4642,9 @@ describe Mongoid::Contextual::Mongo do
   end
 
   describe '#prepare_atomic_updates' do
-    let(:instance) { described_class.new(Band.where(name: 'Depeche Mode')) }
+    subject(:updates) { instance.send(:prepare_atomic_updates, Band, hash) }
 
-    subject { instance.send(:prepare_atomic_updates, Band, hash) }
+    let(:instance) { described_class.new(Band.where(name: 'Depeche Mode')) }
 
     context 'when the hash already contains the key' do
 
@@ -4654,10 +4654,10 @@ describe Mongoid::Contextual::Mongo do
         end
 
         it 'moves the non hash values under the provided key' do
-          expect(subject).to eq({
-                                  '$set' => { 'name' => 'Tool', likes: 10 },
-                                  '$inc' => { 'plays' => 1 }
-                                })
+          expect(updates).to eq({
+            '$set' => { 'name' => 'Tool', 'likes' => 10 },
+            '$inc' => { 'plays' => 1 }
+          })
         end
       end
 
@@ -4667,10 +4667,10 @@ describe Mongoid::Contextual::Mongo do
         end
 
         it 'moves the non hash values under the provided key' do
-          expect(subject).to eq({
-                                  '$set' => { likes: 10, 'name' => 'Tool' },
-                                  '$inc' => { 'plays' => 1 }
-                                })
+          expect(updates).to eq({
+            '$set' => { 'likes' => 10, 'name' => 'Tool' },
+            '$inc' => { 'plays' => 1 }
+          })
         end
       end
     end
@@ -4681,10 +4681,10 @@ describe Mongoid::Contextual::Mongo do
       end
 
       it 'moves the non hash values under the provided key' do
-        expect(subject).to eq({
-                                '$set' => { likes: 10, name: 'Tool' },
-                                '$inc' => { 'plays' => 1 }
-                              })
+        expect(updates).to eq({
+          '$set' => { 'likes' => 10, 'name' => 'Tool' },
+          '$inc' => { 'plays' => 1 }
+        })
       end
     end
   end
