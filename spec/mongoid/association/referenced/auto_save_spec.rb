5f2520c6eb112f6d88f9d610d71dfb3c505d75c7
Anton Savitskiy <savick.ant@gmail.com>
2024-05-03 18:23:17 +0200

    fix minimal example

--- spec/mongoid/association/referenced/auto_save_spec.rb
+++ spec/mongoid/association/referenced/auto_save_spec.rb
@@ -11,14 +11,14 @@ describe Mongoid::Association::Referenced::AutoSave do
     before(:all) do
       puts '[AUTOSAVE] BEFORE ALL'
       # require 'pry'; binding.pry
-      Person.has_many :drugs, validate: false, autosave: true
-      Person.has_one :account, validate: false, autosave: true
+      PersonAuto.has_many :drugs, validate: false, autosave: true
+      PersonAuto.has_one :account, validate: false, autosave: true
       # require 'pry'; binding.pry
     end
 
     after(:all) do
       puts '[AUTOSAVE] AFTER ALL'
-      Person.reset_callbacks(:save)
+      PersonAuto.reset_callbacks(:save)
     end
 
     it 'doesnt matter' do
